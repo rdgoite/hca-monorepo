9a3443e56953bd59114f2438d50cc0823afd782d
Alegria Aclan <aaclan@ebi.ac.uk>
2017-09-12 15:16:58 +0100

    Use environment variables for urls

--- accession-app.py
+++ accession-app.py
@@ -8,11 +8,10 @@ import config
 if __name__ == '__main__':
 
     ingest_api = IngestApi(ingest_url=config.INGEST_API_URL)
-    print config.INGEST_API_URL
+    
     accession_processor = AccessionProcessor(ingest_api=ingest_api)
 
-    MessageReceiver(host=config.RABBITMQ_HOST,
-                    port=config.RABBITMQ_PORT,
+    MessageReceiver(url=config.RABBITMQ_URL,
                     queue=config.RABBITMQ_ACCESSION_QUEUE,
                     message_processor=accession_processor) 
 
