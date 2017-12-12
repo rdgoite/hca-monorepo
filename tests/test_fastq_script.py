20aa45950e932448149beb9b747365f94e828435
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2017-12-12 13:50:31 +0000

    Setup code for handling invalid report.

--- tests/test_fastq_script.py
+++ tests/test_fastq_script.py
@@ -31,3 +31,18 @@ class TestFastqScript(TestCase):
             stdout_value = mock_stdout.getvalue().strip()
             json_output = json.loads(stdout_value)
             self.assertEqual("VALID", json_output["validation_state"])
+
+    def test_prints_out_invalid_report(self):
+        with patch('validator.fastq.Validator') as validator:
+            # given:
+            validator.validate.return_value = ValidationReport("INVALID")
+
+            # and:
+            fastq = Fastq(validator)
+
+            # when:
+            path_to_file = "path/to/file.fastq"
+            fastq.executeOn(path_to_file)
+
+            # then:
+            validator.validate.assert_called_once_with(path_to_file)
