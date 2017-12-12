e6a180a23c22fb5e69dd24c6f3362e2684d77eb4
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2017-12-12 16:30:55 +0000

    Defined main script for running FASTQ validation.

--- script/fastq.py
+++ script/fastq.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
 import json
+import sys
+
+from validator.fastq import Validator
 
 
 class Fastq:
@@ -10,3 +15,8 @@ class Fastq:
         report = self.validator.validate(file_path)
         json_value = json.dumps(report.__dict__)
         print(json_value)
+
+
+if __name__ == '__main__':
+    validator = Validator()
+    Fastq(validator).executeOn(sys.argv[1])
