0fe88a2ae9180546e52bb36eef93272912a463bd
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2017-12-18 16:44:14 +0000

    Defined a specialization of ValidationError for records.

--- common/validation_report.py
+++ common/validation_report.py
@@ -3,6 +3,12 @@ class ValidationError:
     def __init__(self, user_friendly_message):
         self.user_friendly_message = user_friendly_message
 
+class RecordValidationError(ValidationError):
+
+    def __init__(self, sequence_id):
+        self.sequence_id = sequence_id
+        self.user_friendly_message = "Invalid record with sequence id [%s]." % (sequence_id)
+
 class ValidationReport:
 
     def __init__(self, state="", error_reports=None):
@@ -14,6 +20,9 @@ class ValidationReport:
 
     def log_error(self, user_friendly_message):
         self.errors.append(ValidationError(user_friendly_message))
+
+    def log_record_error(self, sequence_id):
+        self.errors.append(RecordValidationError(sequence_id))
 
     def to_dict(self):
         return {
